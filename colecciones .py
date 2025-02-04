34978024a3069cc1aad0b1011770612285692e00
aydeelo <juanaide1129@gmail.com>
2025-02-04 08:27:08 -0500

    listas

---
+++ colecciones .py
@@ -0,0 +1,46 @@
+"""
+edad = 27
+lista1 = ["Aydee", "Roldán", 27, True,"hello"] 
+print(len(lista1))
+print(lista1[1])
+"""
+
+"""
+frutas = ["fresa", "papaya", "pera"]
+verduras = ["zanahoria", "lechuga", "tomate"]
+frutas.append("mango")
+print(frutas)
+frutas.insert(1, "mandarina")
+print(frutas)
+frutas[1]="kiwi"
+print(frutas)
+frutas.extend(verduras)
+print(frutas)
+#frutas.clear()
+frutas2 = frutas.copy()
+print(frutas)
+print(frutas2)
+frutas2.append("toronja")
+print(frutas2)
+print(frutas)
+"""
+frutas = ["fresa", "papaya", "pera", "papaya"]
+
+"""print(frutas.count("papaya"))
+print(frutas.index("pera"))"""
+
+"""frutas.pop()
+print(frutas)
+frutas.pop(1)
+print(frutas)
+frutas.remove("fresa") 
+print(frutas)
+"""
+
+frutas = ["fresa", "papaya", "pera","papaya" ]
+print(frutas)
+frutas.reverse()
+print(frutas)
+frutas.sort()
+print(frutas)
+print(frutas)
